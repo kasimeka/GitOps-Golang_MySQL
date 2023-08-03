ba5d9d41e972976115809cbcd1af6d5220ede55f
janw4ld <janw4ld@tutanota.com>
2023-08-03 21:28:36 +0300

    Revert "switch flake to non-nixos repo"
    This reverts commit 117bcc587e9a9199d1d94e215c7dc3626c242854.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # in flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem
