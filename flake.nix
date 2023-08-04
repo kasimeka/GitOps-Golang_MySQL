aae82267b5697c8a591bc8cb5a45b645c622bc96
janw4ld <janw4ld@tutanota.com>
2023-08-04 14:18:22 +0300

    finally switched to the suitable nix branch

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
-# in flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.05-darwin";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem
